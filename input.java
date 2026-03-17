748983ab79bbd0f1869523adf462b59c01dc5334
Sarthak9149 <168849656+Sarthak9149@users.noreply.github.com>
2026-03-17 18:40:16 +0530

    taking input from user

---
+++ input.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+// next -> grab the first word of the input
+// nextLine -> grab the whole line of the input
+public class input {
+    public static void main(String[] args) {
+        String name;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        name = sc.nextLine();
+        System.out.println("Your name is: " + name);
+        sc.close();
+    }
+}
